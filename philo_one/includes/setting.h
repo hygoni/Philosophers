33c7a184b557bd0d2812cf8c1ac8dd88c29c8420
hyeyoo <hyeyoo@student.42seoul.kr>
2020-08-17 12:52:23 +0900

    norm fix on philo_one

--- philo_one/includes/setting.h
+++ philo_one/includes/setting.h
@@ -6,7 +6,7 @@
 /*   By: hyeyoo <hyeyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 08:39:16 by hyeyoo            #+#    #+#             */
-/*   Updated: 2020/08/16 05:26:39 by hyeyoo           ###   ########.fr       */
+/*   Updated: 2020/08/17 12:52:14 by hyeyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@
 
 typedef struct			s_data
 {
-	int				size;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				times_must_eat;
-	int				philo_idx;
+	int					size;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					times_must_eat;
+	int					philo_idx;
 	pthread_mutex_t		*mutex;
 	pthread_mutex_t		io_lock;
-}				t_data;
+}						t_data;
 
-typedef struct		s_philo
+typedef struct			s_philo
 {
 	pthread_mutex_t		*left;
 	pthread_mutex_t		*right;
-	int				idx;
-	uint64_t		last_eat_time;
-}			t_philo;
+	int					idx;
+	uint64_t			last_eat_time;
+}						t_philo;
 
 #endif
